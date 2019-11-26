b5d2f28757a41cb0b96862a7191f81138f52408c
Christian Spinelli <christian.spinelli@hibo.it>
2019-11-26 12:34:51 +0100

    init theme script

---
+++ development/themes/init-theme.bat
@@ -0,0 +1,48 @@
+
+SET ERROR=0
+
+IF [%1] == [] (
+  ECHO [ERRO] Theme name parameter needed.
+  EXIT 1
+) ELSE (
+  IF [%1] == ".template" (
+    ECHO [ERRO] Invalid theme name.
+  ) ELSE (
+    IF EXIST %1 (
+
+      ECHO [ERRO] Theme already exists.
+      EXIT 1
+
+    ) ELSE (
+
+      ECHO [INFO] Cloning template theme...
+      Xcopy /E /I ".template" "%1"
+      IF %ERRORLEVEL% NEQ 0 ( SET ERROR=%ERRORLEVEL% )
+      powershell -Command "(gc %1\drussets.config.json) -replace '{{theme-name}}', '%1' | Out-File -encoding ASCII %1\drussets.config.json"
+      IF %ERRORLEVEL% NEQ 0 ( SET ERROR=%ERRORLEVEL% )
+
+      cd "%1\src\"
+      IF %ERRORLEVEL% NEQ 0 ( SET ERROR=%ERRORLEVEL% )
+      ren "info.yml" "%1.info.yml"
+      IF %ERRORLEVEL% NEQ 0 ( SET ERROR=%ERRORLEVEL% )
+      ren "libraries.yml" "%1.libraries.yml"
+      IF %ERRORLEVEL% NEQ 0 ( SET ERROR=%ERRORLEVEL% )
+      ren ".theme" "%1.theme"
+      IF %ERRORLEVEL% NEQ 0 ( SET ERROR=%ERRORLEVEL% )
+
+      powershell -Command "(gc %1.theme) -replace 'theme_', '%1_' | Out-File -encoding ASCII %1.theme"
+      IF %ERRORLEVEL% NEQ 0 ( SET ERROR=%ERRORLEVEL% )
+      powershell -Command "(gc %1.info.yml) -replace '{{theme-name}}', '%1' | Out-File -encoding ASCII %1.info.yml"
+      IF %ERRORLEVEL% NEQ 0 ( SET ERROR=%ERRORLEVEL% )
+
+      IF %ERROR% EQU 0  (
+        ECHO [.OK.] All operations succesfully completed.
+        exit 0
+      ) ELSE (
+        EXIT 1
+      )
+
+    )
+  )
+)
+
